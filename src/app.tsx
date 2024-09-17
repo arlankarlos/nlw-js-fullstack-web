c8dda2c7d9d70d205210e1964be3622bdfe0aeaf
arlankarlos <lolozo@msn.com>
2024-09-17 04:00:09 -0300

    instalado tanstack query

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,41 @@
+import { useEffect, useState } from 'react'
 import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
 import { Summary } from './components/summary'
+import { EmptyGoals } from './components/empty-goals'
 // import { EmptyGoals } from './components/empty-goals'
 
+type SummaryResponse = {
+    completed: number
+    total: number
+    goalsPerDay: Record<
+        string,
+        {
+            id: string
+            title: string
+            completedAt: string
+        }[]
+    >
+}
+
 export function App() {
+    const [summary, setSummary] = useState<SummaryResponse | null>(null)
+
+    useEffect(() => {
+        fetch('http://localhost:3333/summary')
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                setSummary(data.summary)
+            })
+    }, [])
+
     return (
         <Dialog>
+            {summary && summary?.total > 0 ? <Summary /> : <EmptyGoals />}
             {/* <EmptyGoals /> */}
-            <Summary />
+            {/* <Summary /> */}
             <CreateGoal />
         </Dialog>
     )
